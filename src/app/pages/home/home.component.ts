0bffb1456a1a64a7a1d2835356f7dc2702eb340c
web-gardener <zhulishen0331@gmail.com>
2023-03-03 16:40:32 -0500

    [ADD] simple style responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  onSave() {
+
+  }
+
+  onSearch() {
+
+  }
 }
